28f472a75b1ad9369d75ac40b20944b52a9bd5cb
Alaaelwardany <lokaelwardany@gmail.com>
2021-12-22 11:07:59 +0200

    hotel

---
+++ src/component/hotelCard.js
@@ -0,0 +1,106 @@
+import React, { Component, useEffect, useState } from "react";
+import {
+  Box,
+  Heading,
+  AspectRatio,
+  Image,
+  Text,
+  Center,
+  HStack,
+  Stack,
+  NativeBaseProvider,
+} from "native-base";
+import { VStack, Divider } from "native-base";
+
+export default function HotelCard({ hotel, navigation }) {
+  // const [hotel, sethotel] = useState(props.hotel);
+  useEffect(() => {}, []);
+  const navigateToDetails = (id) => {
+    navigation.push("details", {
+      id: id,
+    });
+  };
+
+  return (
+    <Box
+      maxW="80"
+      rounded="lg"
+      overflow="hidden"
+      borderColor="coolGray.200"
+      borderWidth="1"
+      _dark={{
+        borderColor: "coolGray.600",
+        backgroundColor: "gray.700",
+      }}
+      _web={{
+        shadow: 2,
+        borderWidth: 0,
+      }}
+      _light={{
+        backgroundColor: "gray.50",
+      }}
+    >
+      <Box>
+        <AspectRatio w="100%" ratio={16 / 9}>
+          <Image source={hotel?.data().image_url} alt="image" />
+        </AspectRatio>
+        <Center
+          bg="violet.500"
+          _dark={{
+            bg: "violet.400",
+          }}
+          _text={{
+            color: "warmGray.50",
+            fontWeight: "700",
+            fontSize: "xs",
+          }}
+          position="absolute"
+          bottom="0"
+          px="3"
+          py="1.5"
+        ></Center>
+      </Box>
+      <Stack p="4" space={3}>
+        <Stack space={2}>
+          <Heading
+            onPress={() => {
+              navigateToDetails(hotel.id);
+            }}
+            size="md"
+            ml="-1"
+          >
+            {hotel.data().name}
+          </Heading>
+          <Text
+            fontSize="xs"
+            _light={{
+              color: "violet.500",
+            }}
+            _dark={{
+              color: "violet.400",
+            }}
+            fontWeight="500"
+            ml="-0.5"
+            mt="-1"
+          >
+            address : {hotel.data().location?.address}
+          </Text>
+        </Stack>
+        <Text fontWeight="400">{hotel.data().About}</Text>
+        <HStack alignItems="center" space={4} justifyContent="space-between">
+          <HStack alignItems="center">
+            <Text
+              color="coolGray.600"
+              _dark={{
+                color: "warmGray.200",
+              }}
+              fontWeight="400"
+            >
+              rate: {hotel.data().rate}
+            </Text>
+          </HStack>
+        </HStack>
+      </Stack>
+    </Box>
+  );
+}
--- src/route/route.js
+++ src/route/route.js
@@ -1,16 +1,25 @@
-// @flow 
-import * as React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import  Home  from '../screens/home';
+// @flow
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "../screens/home";
+import Hotel from "../screens/hotels";
+import HotelCard from "../component/hotelCard";
+import DetailsCard from "../component/detailsCard";
 const RouteComponent = () => {
-    const Navigation = createNativeStackNavigator();
-    return (
-        <NavigationContainer>
-            <Navigation.Navigator>
-                <Navigation.Screen name="home" component={Home} />
-            </Navigation.Navigator>
-        </NavigationContainer>
-    );
+  const Navigation = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Navigation.Navigator>
+        <Navigation.Screen
+          Navigation={Navigation}
+          name="hotel"
+          component={Hotel}
+        />
+        <Navigation.Screen name="details" component={DetailsCard} />
+        <Navigation.Screen name="home" component={Home} />
+      </Navigation.Navigator>
+    </NavigationContainer>
+  );
 };
 export default RouteComponent;
